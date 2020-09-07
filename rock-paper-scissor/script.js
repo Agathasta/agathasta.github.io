c96d89e542504af3131da06bb3177f311c1ad115
agathasta <69153207+Agathasta@users.noreply.github.com>
2020-09-07 15:42:55 +0200

    Refactor js

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -1,81 +1,73 @@
-const main = document.querySelector("main");
+let scoreUser = 0;
+let scoreComputer = 0;
+let roundCount = 1;
 
-let roundText = document.querySelector("#round");
-roundText.addEventListener("click", start);
+const btnStart = document.querySelector("#button");
+const choiceUser = document.querySelector("#choice-user");
+const choiceComputer = document.querySelector("#choice-computer");
+const scoreUserText = document.querySelector("#score-user");
+const scoreComputerText = document.querySelector("#score-computer");
+const winner = document.querySelector("#winner");
 
-function start() {
-    roundText.textContent = "Round 1";
-    main.appendChild(roundText);
-    userScoreText.textContent = 0;
-    computerScoreText.textContent = 0;
-    userScore = 0;
-    computerScore = 0;
-    winText.textContent = "";
+// set everything to 0
+btnStart.addEventListener("click", startGame);
+function startGame() {
+    btnStart.textContent = "Round 1";
+    choiceUser.textContent = "";
+    choiceComputer.textContent = "";
+    scoreUserText.textContent = 0;
+    scoreComputerText.textContent = 0;
+    winner.textContent = "";
 }
 
-// Define computer choice, user choice and start playing:
+// choice computer
+function playComputer() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    choiceComputer.textContent = "Computer chooses " + choices[Math.floor(Math.random() * choices.length)];
+}
+// choice user
 const btns = document.querySelectorAll(".icon");
 btns.forEach(button => button.addEventListener("click", function () {
-    computerPlay();
+    playComputer();
     let option = button.id.charAt(0).toUpperCase() + button.id.slice(1);
-    let userChoice = document.querySelector("#user-choice");
-    userChoice.textContent = "You choose " + option;
-    playFiveRounds();
+    choiceUser.textContent = "You choose " + option;
+    playRound();
 }));
 
-// Declare a function computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-function computerPlay() {
-    const choices = ["Rock", "Paper", "Scissors"];
-    let computerChoice = document.querySelector("#computer-choice");
-    computerChoice.textContent = "Computer chooses " + choices[Math.floor(Math.random() * 3)];
-}
-// Stop the game after 5 rounds
-let rounds = 2;
-let userScore = 0;
-let userScoreText = document.querySelector("#user-score");
-let computerScore = 0;
-let computerScoreText = document.querySelector("#computer-score");
-let winText = document.querySelector("#winner");
-
-function playFiveRounds() {
-    if (rounds < 6) {
-        playRound();
-    }
-    else if (rounds >= 6) {
-        playRound();
-        computerScore < userScore
-            ? winText.textContent = "YOU WIN!!!!!"
-            : winText.textContent = "YOU LOOSE... OH WELL";
-        main.appendChild(winText);
-        roundText.textContent = "START OVER";
-        main.appendChild(roundText);
-        rounds = 2;
-    }
-}
-// Play one round. Compare both choices, declare if winner or loser and why. Actualize user score
+// play round
 function playRound() {
-    let userChoice = document.querySelector("#user-choice").textContent.split(" ").pop();
-    let computerChoice = document.querySelector("#computer-choice").textContent.split(" ").pop();
-    let result = computerChoice + " " + userChoice;
-
-    if (computerChoice === userChoice) {
-        winText.textContent = "You're tied, try again";
-        winText.setAttribute("style", "color: black")
-    } else if (
-        result === "Rock Paper" ||
-        result === "Paper Scissors" ||
-        result === "Scissors Rock"
-    ) {
-        roundText.textContent = "Round " + rounds++;
-        winText.textContent = `You win, ${userChoice} beats ${computerChoice}`;
-        userScore++;
-        winText.setAttribute("style", "color: #4F5D2F")
-        userScoreText.textContent = userScore;
+    console.log(roundCount);
+    if (roundCount > 5) {
+        scoreComputer < scoreUser
+            ? winner.textContent = "YOU WIN!!!!!"
+            : winner.textContent = "YOU LOOSE... OH WELL";
+        btnStart.textContent = "START OVER";
+        //icons.setAttribute("style", "display: none");
     } else {
-        roundText.textContent = "Round " + rounds++;
-        winText.textContent = `You loose, ${computerChoice} beats ${userChoice}`;
-        computerScore++;
-        winText.setAttribute("style", "color: #B53C17")
-        computerScoreText.textContent = computerScore;
+        let user = choiceUser.textContent.split(" ").pop();
+        let computer = choiceComputer.textContent.split(" ").pop();
+        let result = computer + " " + user;
+        if (computer === user) {
+            winner.textContent = "You're tied, try again";
+            winner.setAttribute("style", "color: black");
+        } else if (
+            result === "Rock Paper" ||
+            result === "Paper Scissors" ||
+            result === "Scissors Rock"
+        ) {
+            roundCount++;
+            roundCount <= 5 ? btnStart.textContent = "Round " + roundCount : "";
+            winner.textContent = `You win, ${user} beats ${computer}`;
+            winner.setAttribute("style", "color: #4F5D2F");
+            scoreUser++;
+            scoreUserText.textContent = scoreUser;
+        } else {
+            roundCount++;
+            roundCount <= 5 ? btnStart.textContent = "Round " + roundCount : "";
+            winner.textContent = `You loose, ${computer} beats ${user}`;
+            winner.setAttribute("style", "color: #B53C17");
+            scoreComputer++;
+            scoreComputerText.textContent = scoreComputer;
+        }
     }
 }
