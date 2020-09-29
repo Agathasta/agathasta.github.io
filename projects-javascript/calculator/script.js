fcc73bc2bf5541b1bb0b8e9d5c046114f22b84a5
agathasta <69153207+Agathasta@users.noreply.github.com>
2020-09-29 18:27:34 +0200

    Finish v.1.0

--- projects-javascript/calculator/script.js
+++ projects-javascript/calculator/script.js
@@ -2,21 +2,28 @@ const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const displayNum = document.querySelector("#display-num");
 const displayOp = document.querySelector("#display-op");
-let number; // stores number
+let num1; // stores number 1
 let operator; // stores the operator
-let result;
+let result; // stores the result
+let calculateResult = false; // check if an operation should be performed
 let clearDisplay = false // check if display needs to be cleared
+let numLock = true;
+let opLock = false;
 
 // display numbers
 for (i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener("click", displayNumbers)
 }
 function displayNumbers(n) {
-    if (clearDisplay) {
-        displayNum.textContent = "";
+    if (numLock) {
+        if (clearDisplay) {
+            num1 = displayNum.textContent;
+            displayNum.textContent = "";
+        }
+        displayNum.textContent += n.target.value;
+        clearDisplay = false;
+        opLock = true;
     }
-    displayNum.textContent += n.target.value;
-    clearDisplay = false;
 }
 
 // display operators
@@ -24,6 +31,30 @@ for (i = 0; i < operators.length; i++) {
     operators[i].addEventListener("click", storeOperator);
 }
 function storeOperator(o) {
-    clearDisplay = true;
-    displayOp.textContent = o.target.value;
+    if (opLock) {
+        displayOp.textContent = o.target.value;
+        opLock = false;
+        if (calculateResult) {
+            operate(num1, displayNum.textContent, operator);
+        }
+        operator = displayOp.textContent;
+        calculateResult = true;
+        clearDisplay = true;
+        operator === "=" ? numLock = false : numLock = true;
+
+    }
+}
+
+function operate(num1, num2, operator) {
+    if (operator === "+") {
+        result = Number(num1) + Number(num2);
+    } else if (operator === "-") {
+        result = Number(num1) - Number(num2);
+    } else if (operator === "*") {
+        result = Number(num1) * Number(num2);
+    } else if (operator === "/") {
+        displayNum.textContent == 0 ? result = "b" + "a" + +"a" + "a" : result = Number(num1) / Number(num2);
+    }
+    displayNum.textContent = result;
+    opLock = true;
 }
