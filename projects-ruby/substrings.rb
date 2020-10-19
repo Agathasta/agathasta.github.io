8eb98135381e0721eb854b931083da7c437fc539
agathasta <kalidonia@hotmail.com>
2020-10-19 21:49:01 +0200

    Finish substrings v.2.0

--- projects-ruby/substrings.rb
+++ projects-ruby/substrings.rb
@@ -1,13 +1,23 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(str, dictionary)
+def substrings(sentence, dictionary)
+
+    words = sentence.downcase.split(" ")
     result = Hash.new(0)
-    dictionary.each do |word|
-        if str.include?(word)
-            result[word] = dictionary.count(word)
+
+    words.each do |word|
+        dictionary.each do |item|
+            if word.include?(item)
+                result[item] += 1
+            end
         end
     end
+
 return result
 end
 
 substrings("below", dictionary)
+#=> {"below"=>1, "low"=>1}
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+# => {"how"=>2, "howdy"=>1, "part"=>1, "partner"=>1, "it"=>2, "i"=>3, "sit"=>1, "down"=>1, "own"=>1, "go"=>1, "going"=>1}
